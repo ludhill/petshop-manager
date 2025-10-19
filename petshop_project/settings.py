1fd91456d6cd281f9bbee0f13b4f38d5d4276761
Ludhill <38694381+ludhill@users.noreply.github.com>
2025-10-19 03:01:52 +0000

    apenas assumir que é UTF-8, que é o padrão universal.

--- petshop_project/settings.py
+++ petshop_project/settings.py
@@ -88,6 +88,10 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
+        # ADICIONE ESTAS 3 LINHAS ABAIXO:
+        'OPTIONS': {
+            'client_encoding': 'UTF8',
+        },
     }
 }
 
